8a1746d0f937b94b5b96c721c026f10bee3ac694
natasha.carlyon@gmail.com <natasha.carlyon@gmail.com>
2020-06-06 10:19:46 -0400

    changes to one

--- one.js
+++ one.js
@@ -2,6 +2,16 @@ function one() {
     console.writeline('eeafe');
 }
 
+function middle() {
+    console.writeline("feafe");
+}
+
 function two() {
     console.writeline("feafe");
+    console.writeline("bbbb");
+    console.writeline("cccc");
+}
+
+function three() {
+    console.writeline("fewef");
 }
