4d248eb3c925e74907b4225ae7c30f6427b9fc65
natasha.carlyon@gmail.com <natasha.carlyon@gmail.com>
2020-06-06 10:22:10 -0400

    resolve merge conflicts

--- one.js
+++ one.js
@@ -2,7 +2,17 @@ function one() {
     console.writeline('eeafe');
 }
 
+function middle() {
+    console.writeline("feafe");
+}
+
+function two() {
+    console.writeline("feafe");
+    console.writeline("bbbb");
+    console.writeline("cccc");
+}
+
 function three() {
     console.writeline("aaaa");
-    console.writeline("eafe");
+    console.writeline("fewef");
 }
